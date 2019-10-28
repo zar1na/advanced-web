827fa692ab4c836359ca341c6a7ffccf89a8e948
zar1na <Z.ALI2269@student.leedsbeckett.ac.uk>
2019-10-28 14:53:18 +0000

    Database progress week 5

---
+++ app/Project.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    //mvc - model view controller *represent building blocks
+    // artisan tinker-> run basic php or reference classes
+    // collections are glorified arrays
+}
--- database/migrations/2019_10_28_141821_create_projects_table.php
+++ database/migrations/2019_10_28_141821_create_projects_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateProjectsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('projects', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('username');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('title');
+            $table->text('description');
             $table->timestamps();
         });
     }
+    
+    // migrate:fresh just drops and runs up method for every class
 
     /**
      * Reverse the migrations.
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('projects');
     }
 }
